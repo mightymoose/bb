b8a12af5b516be1602a14efb0272aa6eef46d872
Ryan Morris <mryan.morris@gmail.com>
2021-03-13 10:51:38 -0600

    feat(cli): add the ability to parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Debug, StructOpt)]
+struct Arguments {
+    #[structopt(long)]
+    log_in: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = Arguments::from_args();
+    println!("{:?}", arguments);
 }
